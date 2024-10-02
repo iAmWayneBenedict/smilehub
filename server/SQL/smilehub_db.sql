2a8c21a98375c128acdac2f2110e674eab861966
Kharl Samson <khrlsmsn1110@gmail.com>
2024-10-02 21:33:29 +0800

    Updated SQL

--- server/SQL/smilehub_db.sql
+++ server/SQL/smilehub_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: mysql
--- Generation Time: Sep 28, 2024 at 12:14 PM
+-- Generation Time: Oct 02, 2024 at 01:32 PM
 -- Server version: 8.0.39
 -- PHP Version: 8.2.8
 
@@ -44,8 +44,8 @@ CREATE TABLE `appointment_table` (
 
 INSERT INTO `appointment_table` (`ID`, `FULLNAME`, `EMAIL`, `PHONE`, `APPOINTMENT_DATE`, `APPOINTMENT_TIME`, `PURPOSE`, `STATUS`) VALUES
 (8, 'Kharl Samson', 'khrlsmsn1110@gmail.com', '9121212121', '2024-08-11', '8:00 AM - 9:00 AM', 'Cleaning', 'Completed'),
-(43, 'Kharl Samson', 'khrlsmsn1110@gmail.com', '9012121211', '2024-09-22', '9:00 PM - 10:00 PM', 'Cleaning', 'Pending'),
-(46, 'John Doe', 'john.doe@example.com', '555-1234', '2024-09-23', '10:00 AM - 11:00 AM', 'Dental Crowns', 'Pending');
+(46, 'John Doe', 'john.doe@example.com', '555-1234', '2024-09-23', '10:00 AM - 11:00 AM', 'Dental Crowns', 'Completed'),
+(47, 'John Doe', 'john.doe@example.com', '555-1234', '2024-10-03', '8:00 AM - 9:00 AM', 'Dentures', 'Pending');
 
 -- --------------------------------------------------------
 
@@ -82,18 +82,25 @@ INSERT INTO `appointment_time_table` (`ID`, `TIME`) VALUES
 
 CREATE TABLE `assessment_table` (
   `ID` int NOT NULL,
-  `PATIENT_ID` varchar(100) NOT NULL,
-  `TOOTH_NO` varchar(20) NOT NULL,
-  `COLOR` varchar(20) NOT NULL,
-  `TEXTURE` varchar(50) NOT NULL,
-  `GUM_HEALTH` varchar(50) NOT NULL,
-  `PRESENCE_OF_DECAY` varchar(10) NOT NULL,
-  `CAVITIES` varchar(10) NOT NULL,
-  `SENSITIVITY` varchar(10) NOT NULL,
-  `MOBILITY` varchar(50) NOT NULL,
-  `PREVIOUS_TREATMENT` varchar(50) NOT NULL,
+  `PATIENT_ID` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `TOOTH_NO` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
+  `COLOR` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
+  `TEXTURE` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `GUM_HEALTH` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `PRESENCE_OF_DECAY` varchar(10) COLLATE utf8mb4_general_ci NOT NULL,
+  `CAVITIES` varchar(10) COLLATE utf8mb4_general_ci NOT NULL,
+  `SENSITIVITY` varchar(10) COLLATE utf8mb4_general_ci NOT NULL,
+  `MOBILITY` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `PREVIOUS_TREATMENT` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
   `DATETIME` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `assessment_table`
+--
+
+INSERT INTO `assessment_table` (`ID`, `PATIENT_ID`, `TOOTH_NO`, `COLOR`, `TEXTURE`, `GUM_HEALTH`, `PRESENCE_OF_DECAY`, `CAVITIES`, `SENSITIVITY`, `MOBILITY`, `PREVIOUS_TREATMENT`, `DATETIME`) VALUES
+(9, '11', '1', 'Brown', 'Pitted', 'Receding', 'no', 'yes', 'no', 'Moderately Loose', 'Extraction', '2024-10-02 12:14:16');
 
 -- --------------------------------------------------------
 
@@ -131,10 +138,20 @@ INSERT INTO `employee_table` (`ID`, `FULLNAME`, `EMAIL`, `BIRTHDAY`, `GENDER`, `
 
 CREATE TABLE `file_assessment_table` (
   `ID` int NOT NULL,
-  `PATIENT_ID` varchar(100) NOT NULL,
-  `FILENAME` longtext NOT NULL,
+  `PATIENT_ID` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `URL` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `DATETIME` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `file_assessment_table`
+--
+
+INSERT INTO `file_assessment_table` (`ID`, `PATIENT_ID`, `URL`, `DATETIME`) VALUES
+(13, '15', '035f86b401af16c24ee7f9c250a6ae72_1.png', '2024-10-02 12:07:41'),
+(14, '11', '21e2d7b8c59b33d5e403b38acfcda0fe_1.png', '2024-10-02 12:13:58'),
+(15, '15', 'e3ca06eab81406de22d5d900765ec054_1.png', '2024-10-02 12:19:25'),
+(16, '12345', 'test', '2024-10-02 12:34:50');
 
 -- --------------------------------------------------------
 
@@ -144,35 +161,42 @@ CREATE TABLE `file_assessment_table` (
 
 CREATE TABLE `form_table` (
   `ID` int NOT NULL,
-  `PATIENT_ID` varchar(100) NOT NULL,
-  `TITLE` varchar(30) NOT NULL,
-  `FIRST_NAME` varchar(75) NOT NULL,
-  `LAST_NAME` varchar(75) NOT NULL,
-  `OCCUPATION` varchar(100) NOT NULL,
-  `BIRTHDAY` varchar(100) NOT NULL,
-  `HOME_ADDRESS` varchar(250) NOT NULL,
-  `CONTACT_NUMBER` varchar(50) NOT NULL,
-  `EMAIL_ADDRESS` varchar(100) NOT NULL,
-  `HEALTH_FUND` varchar(250) NOT NULL,
-  `MEMBER_NUMBER` varchar(50) NOT NULL,
-  `EMERGENCY_CONTACT_NAME` varchar(75) NOT NULL,
-  `EMERGENCY_CONTACT_NUMBER` varchar(50) NOT NULL,
-  `EMERGENCY_CONTACT_RELATIONSHIP` varchar(50) NOT NULL,
-  `FAMILY_DOCTOR` varchar(75) NOT NULL,
-  `DOCTOR_CONTACT` varchar(50) NOT NULL,
-  `SUFFERING` longtext NOT NULL,
-  `PREGNANT_DURATION` longtext NOT NULL,
-  `HOSPITAL_PAST_2_DURATION` longtext NOT NULL,
-  `MEDICATION` longtext NOT NULL,
-  `SMOKE_PER_DAY` longtext NOT NULL,
-  `DENTAL_CONCERN_PROBLEMS` longtext NOT NULL,
-  `VISIT_PURPOSE` longtext NOT NULL,
+  `PATIENT_ID` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `TITLE` varchar(30) COLLATE utf8mb4_general_ci NOT NULL,
+  `FIRST_NAME` varchar(75) COLLATE utf8mb4_general_ci NOT NULL,
+  `LAST_NAME` varchar(75) COLLATE utf8mb4_general_ci NOT NULL,
+  `OCCUPATION` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `BIRTHDAY` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `HOME_ADDRESS` varchar(250) COLLATE utf8mb4_general_ci NOT NULL,
+  `CONTACT_NUMBER` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `EMAIL_ADDRESS` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `HEALTH_FUND` varchar(250) COLLATE utf8mb4_general_ci NOT NULL,
+  `MEMBER_NUMBER` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `EMERGENCY_CONTACT_NAME` varchar(75) COLLATE utf8mb4_general_ci NOT NULL,
+  `EMERGENCY_CONTACT_NUMBER` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `EMERGENCY_CONTACT_RELATIONSHIP` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `FAMILY_DOCTOR` varchar(75) COLLATE utf8mb4_general_ci NOT NULL,
+  `DOCTOR_CONTACT` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `SUFFERING` longtext COLLATE utf8mb4_general_ci NOT NULL,
+  `PREGNANT_DURATION` longtext COLLATE utf8mb4_general_ci NOT NULL,
+  `HOSPITAL_PAST_2_DURATION` longtext COLLATE utf8mb4_general_ci NOT NULL,
+  `MEDICATION` longtext COLLATE utf8mb4_general_ci NOT NULL,
+  `SMOKE_PER_DAY` longtext COLLATE utf8mb4_general_ci NOT NULL,
+  `DENTAL_CONCERN_PROBLEMS` longtext COLLATE utf8mb4_general_ci NOT NULL,
+  `VISIT_PURPOSE` longtext COLLATE utf8mb4_general_ci NOT NULL,
   `LAST_DENTAL` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `MAKE_YOU_NERVOUS` longtext NOT NULL,
-  `DENTAL_TREATMENT_REQUIREMENT` longtext NOT NULL,
-  `REFFERAL` varchar(50) NOT NULL,
+  `MAKE_YOU_NERVOUS` longtext COLLATE utf8mb4_general_ci NOT NULL,
+  `DENTAL_TREATMENT_REQUIREMENT` longtext COLLATE utf8mb4_general_ci NOT NULL,
+  `REFFERAL` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
   `DATETIME` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `form_table`
+--
+
+INSERT INTO `form_table` (`ID`, `PATIENT_ID`, `TITLE`, `FIRST_NAME`, `LAST_NAME`, `OCCUPATION`, `BIRTHDAY`, `HOME_ADDRESS`, `CONTACT_NUMBER`, `EMAIL_ADDRESS`, `HEALTH_FUND`, `MEMBER_NUMBER`, `EMERGENCY_CONTACT_NAME`, `EMERGENCY_CONTACT_NUMBER`, `EMERGENCY_CONTACT_RELATIONSHIP`, `FAMILY_DOCTOR`, `DOCTOR_CONTACT`, `SUFFERING`, `PREGNANT_DURATION`, `HOSPITAL_PAST_2_DURATION`, `MEDICATION`, `SMOKE_PER_DAY`, `DENTAL_CONCERN_PROBLEMS`, `VISIT_PURPOSE`, `LAST_DENTAL`, `MAKE_YOU_NERVOUS`, `DENTAL_TREATMENT_REQUIREMENT`, `REFFERAL`, `DATETIME`) VALUES
+(3, '15', 'Mr.', 'test', 'test', 'test', 'N/A', 'test', '09197174114', 'test@gmail.com', 'test', 'test', 'test', 'N/A', 'test', 'test', '09197174114', 'Bone Disease (Osteoporosis)', 'test', 'test', 'test', 'test', 'Array', 'test', '2024-10-02', 'Yes', 'N/A', 'Array', '2024-10-02 13:17:56');
 
 -- --------------------------------------------------------
 
@@ -192,7 +216,7 @@ CREATE TABLE `inventory_group_table` (
 
 INSERT INTO `inventory_group_table` (`ID`, `NAME`, `DATETIME`) VALUES
 (1, 'Dental Care Equipment A', '2024-09-22 13:14:00'),
-(5, 'Dental Care Equipment B', '2024-09-22 13:14:00');
+(7, 'Dental Care Equipment B', '2024-10-02 13:30:11');
 
 -- --------------------------------------------------------
 
@@ -217,7 +241,8 @@ INSERT INTO `inventory_item_table` (`ID`, `NAME`, `ITEM_GROUP`, `LOCATION`, `QUA
 (4, 'Updated Item Name A', 'Dental Care Equipment A', 'Updated Storage Location', '20', '2024-09-22 13:53:18'),
 (7, 'Updated Item Name B', 'Dental Care Equipment B', 'Updated Storage Location', '9', '2024-09-22 13:51:28'),
 (8, 'Updated Item Name C', 'Dental Care Equipment B', 'Updated Storage Location', '20', '2024-09-22 13:53:18'),
-(9, 'Updated Item Name D', 'Dental Care Equipment A', 'Updated Storage Location', '20', '2024-09-22 13:51:28');
+(9, 'Updated Item Name D', 'Dental Care Equipment A', 'Updated Storage Location', '20', '2024-09-22 13:51:28'),
+(15, 'Item 1', 'Dental Care Equipment B', 'Storage A', '3', '2024-10-02 13:30:11');
 
 -- --------------------------------------------------------
 
@@ -254,17 +279,17 @@ INSERT INTO `patient_table` (`ID`, `FIRSTNAME`, `LASTNAME`, `BIRTHDATE`, `GENDER
 
 CREATE TABLE `progress_table` (
   `ID` int NOT NULL,
-  `PATIENT_ID` varchar(100) NOT NULL,
-  `COMPLAINT` longtext NOT NULL,
-  `HISTORY_UPDATE` longtext NOT NULL,
-  `X_RAY_FILE` varchar(100) NOT NULL,
-  `DIAGNOSIS` longtext NOT NULL,
-  `TREATMENT_PLAN` longtext NOT NULL,
-  `PROCEDURES` longtext NOT NULL,
-  `MATERIALS_USED` longtext NOT NULL,
-  `INSTRUCTION` longtext NOT NULL,
-  `RESPONSE` longtext NOT NULL,
-  `SIGNATURE` varchar(100) NOT NULL,
+  `PATIENT_ID` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `COMPLAINT` longtext COLLATE utf8mb4_general_ci NOT NULL,
+  `HISTORY_UPDATE` longtext COLLATE utf8mb4_general_ci NOT NULL,
+  `X_RAY_FILE` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `DIAGNOSIS` longtext COLLATE utf8mb4_general_ci NOT NULL,
+  `TREATMENT_PLAN` longtext COLLATE utf8mb4_general_ci NOT NULL,
+  `PROCEDURES` longtext COLLATE utf8mb4_general_ci NOT NULL,
+  `MATERIALS_USED` longtext COLLATE utf8mb4_general_ci NOT NULL,
+  `INSTRUCTION` longtext COLLATE utf8mb4_general_ci NOT NULL,
+  `RESPONSE` longtext COLLATE utf8mb4_general_ci NOT NULL,
+  `SIGNATURE` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
   `DATE` datetime DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -362,7 +387,7 @@ ALTER TABLE `task_table`
 -- AUTO_INCREMENT for table `appointment_table`
 --
 ALTER TABLE `appointment_table`
-  MODIFY `ID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
+  MODIFY `ID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
 
 --
 -- AUTO_INCREMENT for table `appointment_time_table`
@@ -374,7 +399,7 @@ ALTER TABLE `appointment_time_table`
 -- AUTO_INCREMENT for table `assessment_table`
 --
 ALTER TABLE `assessment_table`
-  MODIFY `ID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `ID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `employee_table`
@@ -386,25 +411,25 @@ ALTER TABLE `employee_table`
 -- AUTO_INCREMENT for table `file_assessment_table`
 --
 ALTER TABLE `file_assessment_table`
-  MODIFY `ID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `ID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `form_table`
 --
 ALTER TABLE `form_table`
-  MODIFY `ID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `ID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `inventory_group_table`
 --
 ALTER TABLE `inventory_group_table`
-  MODIFY `ID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `ID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `inventory_item_table`
 --
 ALTER TABLE `inventory_item_table`
-  MODIFY `ID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `ID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `patient_table`
